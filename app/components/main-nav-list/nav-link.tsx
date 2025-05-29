64e476f7fe922fbe9c39918f8f7b8d8be1466d7b
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-05-29 20:49:58 +0000

    Change color

--- app/components/main-nav-list/nav-link.tsx
+++ app/components/main-nav-list/nav-link.tsx
@@ -2,7 +2,7 @@ import { MainNavItem } from "@/types"
 
 export default function NavLink({ item }: { item: MainNavItem }) {
   return (
-    <a href={item.href} className="">
+    <a href={item.href} className="text-slate-400">
       {item.title}
     </a>
   )
