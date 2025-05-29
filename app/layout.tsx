bf5b8f7cf8717d205fe23e1de86bb970d18bb760
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-05-29 20:49:28 +0000

    Implement main content skip link

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google"
 
 import Footer from "./components/footer"
 import Header from "./components/header"
+import MainContentSkipLink from "./components/main-content-skip-link"
 import type { Metadata } from "next"
 
 const geistSans = Geist({
@@ -30,11 +31,8 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <a
-          href="#main-content"
-          className="sr-only focus:not-sr-only absolute left-4 top-4 z-50 bg-white text-black p-2 rounded shadow">
-          Skip to main content
-        </a>
+        <MainContentSkipLink />
+
         <div className="grid grid-rows-[20px_1fr_20px] items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
           <Header />
 
