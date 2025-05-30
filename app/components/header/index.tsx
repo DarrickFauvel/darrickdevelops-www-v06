c1d95ff6edf62ffab682cf3c65cb6a805e022712
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-05-30 09:16:41 +0000

    Add mode toggle component

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,11 +1,13 @@
 import Logo from "./logo"
 import MainNavList from "../main-nav-list"
+import { ModeToggle } from "../mode-toggle"
 
 export default function Header() {
   return (
     <header className="row-start-1">
       <nav className="flex justify-between" aria-label="Main navigation">
         <Logo />
+        <ModeToggle />
         <MainNavList />
       </nav>
     </header>
