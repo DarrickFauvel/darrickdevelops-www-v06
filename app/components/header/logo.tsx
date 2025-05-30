bfa41fc25ddae627225cce3be8918bbfc2ff7370
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-05-30 09:16:12 +0000

    Remove text color

--- app/components/header/logo.tsx
+++ app/components/header/logo.tsx
@@ -4,7 +4,7 @@ import { siteConfig } from "@/config/site"
 export default function Logo() {
   return (
     <Link
-      className="font-bold text-slate-50 flex gap-1.5"
+      className="font-bold flex gap-1.5"
       href="/"
       aria-label="Go to homepage - Darrick Develops">
       <span className="text-blue-600">{siteConfig.brackets}</span>
