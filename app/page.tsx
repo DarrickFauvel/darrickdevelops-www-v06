0388240564bf675f204732b6430a4b3802d20938
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-05-31 09:55:57 +0000

    Create and add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import { Hero } from "./sections/hero"
+
 export default function Home() {
   return (
     <>
       <main
         className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start"
         id="main-content">
-        <h1>Main content</h1>
+        <Hero />
       </main>
     </>
   )
