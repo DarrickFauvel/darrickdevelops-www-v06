8d0b696fc00af4eb6e79d3aead322909c50d1488
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-05-31 08:57:11 +0000

    Increase logo size

--- app/components/header/logo.tsx
+++ app/components/header/logo.tsx
@@ -4,7 +4,7 @@ import { siteConfig } from "@/config/site"
 export function Logo() {
   return (
     <Link
-      className="font-bold flex gap-1.5"
+      className="font-bold flex gap-1.5 text-xl"
       href="/"
       aria-label="Go to homepage - Darrick Develops">
       <span className="text-blue-600">{siteConfig.brackets}</span>
