371e76302e87989e041add1260693c2921c618db
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-06-02 09:19:35 +0000

    Move nav element inside footer nav list component

--- app/components/footer/footer-nav-list.tsx
+++ app/components/footer/footer-nav-list.tsx
@@ -2,13 +2,13 @@ import { NavItem } from "../nav-list/nav-item"
 import { siteMainNavItems } from "@/config/site"
 
 export function FooterNavList() {
-  const navListContent = siteMainNavItems.map((item) => {
+  const navListItems = siteMainNavItems.map((item) => {
     return <NavItem item={item} key={item.title} />
   })
 
   return (
-    <>
-      <ul className="flex gap-4">{navListContent}</ul>
-    </>
+    <nav>
+      <ul className="flex gap-4 text-xs">{navListItems}</ul>
+    </nav>
   )
 }
