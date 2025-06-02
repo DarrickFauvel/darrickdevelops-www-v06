ae9a6aa3d558cb93ec82170ecc501d5dbd774fbe
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-06-02 20:10:09 +0000

    Fix spacing

--- app/components/header/mobile-menu.tsx
+++ app/components/header/mobile-menu.tsx
@@ -34,9 +34,7 @@ export function MobileMenu({
                 done. */}
             </SheetDescription>
           </SheetHeader>
-          <div className="grid flex-1 auto-rows-min gap-6 px-4">
-            {navListContent}
-          </div>
+          <ul className="px-4 flex flex-col gap-4">{navListContent}</ul>
           {/* <SheetFooter>
               <Button type="submit">Save changes</Button>
               <SheetClose asChild>
