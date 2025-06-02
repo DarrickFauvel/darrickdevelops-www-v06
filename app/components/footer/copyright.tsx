f3ae1bbaf49960d0b0b59c98304ca293ec05455b
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-06-02 09:19:09 +0000

    Show copyright text at small viewport

--- app/components/footer/copyright.tsx
+++ app/components/footer/copyright.tsx
@@ -4,7 +4,7 @@ import { siteConfig } from "@/config/site"
 export function Copyright() {
   return (
     <div className="text-xs">
-      <span className="hidden md:inline">Copyright </span>
+      <span className="hidden sm:inline">Copyright </span>
       <span>
         &copy; {currentYear()} {siteConfig.name}. All rights reserved.
       </span>
