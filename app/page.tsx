f2122982b48ed6ed03d9d82c85856f08dd46e6d2
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-06-22 19:42:00 +0000

    Insert page sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+import AboutSection from "./sections/about"
+import ContactSection from "./sections/contact"
 import { Hero } from "./sections/hero"
+import PortfolioSection from "./sections/portfolio"
 
 export default function Home() {
   return (
@@ -7,6 +10,12 @@ export default function Home() {
         className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start"
         id="main-content">
         <Hero />
+
+        <AboutSection />
+        
+        <PortfolioSection />
+        
+        <ContactSection />
       </main>
     </>
   )
